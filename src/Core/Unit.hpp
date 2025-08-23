52a8a4e9a7186d2805215ef6005a3aa1f12566a8
Taguhi Abgar <taguhiabgar@gmail.com>
2025-08-23 05:06:53 +0400

    Added Unit abstract class with pure virtual functions: spawn, makeMove, receiveDamage

---
+++ src/Core/Unit.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Core/Stats.hpp"
+#include "Core/Position.hpp"
+
+namespace sw::core {
+
+class Unit {
+public:
+    Unit(int id, const Stats& stats, Position position, bool onBoard)
+        : _id(id), _stats(stats), _position(position), _isOnBoard(onBoard), _isAlive(true) { }
+    virtual ~Unit() = default;
+    
+    virtual void spawn() = 0;
+    virtual void makeMove() = 0;
+    virtual void receiveDamage(int damage) = 0;
+    
+    inline bool isOnBoard() { return _isOnBoard; }
+    inline bool isAlive() { return _isAlive; }
+    
+protected:
+    int _id;
+    Stats _stats;
+    Position _position;
+    bool _isOnBoard;
+    bool _isAlive;
+};
+
+} // namespace sw::core
+
